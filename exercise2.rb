58982fb80884b781619baac1297227fa751e10e1
Yuan He <YuanHe@Jessicas-MacBook-Air.local>
2016-10-18 14:54:14 -0400

    sorry i messed up

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#Question1: I guess 20% makes a good tip
+puts "#{55*0.2}"
+
+#Question2:
+12.to_s
+puts "my favourite number is "+"12"
+
+#Question3
+puts "The Answer to the question in your heart is #{45628*7839}"
+
+#Question4
+#Im guessing the answer is true. lets find out
+puts "#{(10 < 20 && 30 < 20) || !(10 == 11)}"
+#guessed right
---
+++ exercise3.rb
@@ -0,0 +1,3 @@
+puts "What is your name?"
+your_name = gets
+puts your_name
