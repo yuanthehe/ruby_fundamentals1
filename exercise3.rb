a73b5e6d467d05ef8e6b7c6e3bff0a7748f0fc55
Yuan He <YuanHe@Jessicas-MacBook-Air.local>
2016-10-18 15:02:28 -0400

    hoooray

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 your_name = gets.chomp
 puts "Hi #{your_name}!"
+puts "How old are you?"
+guestage = gets.chomp
+puts "Welp, I guess you were born in #{2016-guestage.to_i}"
